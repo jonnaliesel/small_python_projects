e9bc9e42800bcf5d7c9395704de2d5f2ae9bf533
jonnaliesel <jonnaliesel@gmail.com>
2021-10-11 13:57:21 +0200

    refactor evaluation

--- numbergame.py
+++ numbergame.py
@@ -3,20 +3,14 @@ import random
 def numbersgame() :
 
     num = random.randint(1, 10)
+    print(f'\nCheet: {num}')
     tries = 0
     guess = int(input('\nGuess a number between 1 and 10: \nYou have 10 tries to get it right ;) '))
 
     while guess :
         diff = num - guess
-        if diff == 0 and tries < 9:
-            tries += 1
-            if(tries > 1):
-                print(f'\nGood job, you got it right in just {tries} tries!')
-            else:    
-                print(f'\nGood job, you got it right in just {tries} try!')
-            break
             
-        elif diff > 0 and tries < 9:
+        if diff > 0 and tries < 9:
             tries += 1
             guess = int(input(f'\nSorry, the correct answer is bigger then {guess}\nNumber of tries: {tries}\n\nGuess again '))
 
@@ -28,5 +22,10 @@ def numbersgame() :
             print('\n***************GAME OVER***************\nOh no, you ran out of tries!')
             break
 
-        else :
-            guess = input('\nSorry, thats not a number. Try again ')
+        else:
+            tries += 1
+            if(tries > 1):
+                print(f'\nGood job, you got it right in just {tries} tries!')
+            else:    
+                print(f'\nGood job, you got it right in just {tries} try!')
+            break
