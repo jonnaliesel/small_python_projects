cc1bf470eaf46746cc0a95c1ba352216dc9f426e
jonnaliesel <jonnaliesel@gmail.com>
2021-10-11 11:29:23 +0200

    add number of tries

--- numbergame.py
+++ numbergame.py
@@ -5,19 +5,27 @@ def numbersgame() :
     num = random.randint(1, 100)
     print(num)
 
-    guess = int(input('\nGuess a number between 1 and 100: ' ))
-    diff = num - guess
+    tries = 0
+    guess = int(input('\nGuess a number between 1 and 100: \nYou have 10 tries to get it right ;) '))
 
     while guess :
-        if diff == 0 :
-            print('Good job, you got it right!')
+        diff = num - guess
+        if diff == 0 and tries < 9:
+            tries += 1
+            print(f'\nGood job, you got it right in just {tries} tries!')
             break
             
-        elif diff > 0 :
-            print('Sorry, the correct answer is bigger then ' + str(guess))
+        elif diff > 0 and tries < 9:
+            tries += 1
+            guess = int(input(f'\nSorry, the correct answer is bigger then {guess}\nNumber of tries: {tries}\n\nGuess again '))
 
-        elif diff < 0 :
-            print('Sorry, the correct answer is smaller then ' + str(guess))
+        elif diff < 0 and tries < 9:
+            tries += 1
+            guess = int(input(f'\nSorry, the correct answer is smaller then {guess}\nNumber of tries: {tries}\n\nGuess again '))
+
+        elif tries > 8:
+            print('\n***************GAME OVER***************\nOh no, you ran out of tries!')
+            break
 
         else :
-            print('Sorry, thats not a number. Try again')
+            guess = input('\nSorry, thats not a number. Try again ')
